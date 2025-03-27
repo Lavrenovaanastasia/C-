5e5d661540a5264de77d858da57b9803e067c5ab
Lavrenovaanastasia <125998621+Lavrenovaanastasia@users.noreply.github.com>
2025-03-27 12:40:42 +0300

    Add files via upload

---
+++ MCHA/Laba3/lab3/lab3/lab3.cpp
@@ -0,0 +1,160 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double f(double x) {
+    return sin( x * x+1) -0.5 ; // уравнение, для которого ищем корни
+}
+
+double proizvodnai(double x) {
+    return  2 * x * cos( x * x+1); // производная уравнения
+}
+
+int signChanges(double a, double b) {
+    int count = 0;
+    double fa = f(a);
+    double fb;
+
+    for (double x = a + 0.1; x <= b; x += 0.1) {
+        fb = f(x);
+        if (fa * fb < 0) {
+            count++;
+        }
+        fa = fb;
+    }
+
+    return count;
+}
+
+void bisectionMethod(double a, double b, double epsilon) {
+    double c;
+    int iterations = 0;
+
+    if (f(a) * f(b) >= 0) {
+        cout << "Условие f(a) * f(b) < 0 не выполнено." << endl;
+        return;
+    }
+
+    while ((b - a) >= epsilon) {
+        c = (a + b) / 2;
+
+        if (f(c) == 0.0) {
+            break;
+        }
+        else if (f(c) * f(a) < 0) {
+            b = c;
+        }
+        else {
+            a = c;
+        }
+
+        iterations++;
+    }
+
+    cout << "Метод половинного деления: Корень = " << c << ", Число итераций = " << iterations << endl;
+}
+
+void chordMethod(double a, double b, double epsilon) {
+    double x1, x2, x3;
+    int iterations = 0;
+
+    if (f(a) * f(b) >= 0) {
+        cout << "Условие f(a) * f(b) < 0 не выполнено." << endl;
+        return;
+    }
+
+    x1 = a;
+    x2 = b;
+
+    do {
+        x3 = x2 - ((f(x2) * (x2 - x1)) / (f(x2) - f(x1)));
+
+        if (f(x3) == 0.0) {
+            break;
+        }
+        else if (f(x3) * f(x1) < 0) {
+            x2 = x3;
+        }
+        else {
+            x1 = x3;
+        }
+
+        iterations++;
+    } while (fabs(f(x3)) >= epsilon);
+
+    cout << "Метод хорд: Корень = " << x3 << ", Число итераций = " << iterations << endl;
+}
+
+void newtonMethod(double x0, double epsilon) {
+    double x1, x2;
+    int iterations = 0;
+
+    x1 = x0;
+
+    do {
+        x2 = x1 - (f(x1) / proizvodnai(x1));
+
+        if (f(x2) == 0.0) {
+            break;
+        }
+
+        x1 = x2;
+        iterations++;
+    } while (fabs(f(x2)) >= epsilon);
+
+    cout << "Метод Ньютона: Корень = " << x2 << ", Число итераций = " << iterations << endl;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    double a = 0;
+    double b = 10;
+    double epsilon = 0.0001;
+    int numRoots = signChanges(a, b);
+
+    cout << "Число корней на отрезке [-10, 10] = " << numRoots << endl;
+
+    bisectionMethod(a, b, epsilon);
+    chordMethod(a, b, epsilon);
+    newtonMethod(a, epsilon);
+
+    return 0;
+}
+
+
+/*
+double f(double x) {
+    return x * x * x + 38.4621 * x * x + 364.594 * x + 914.196; // уравнение, для которого ищем корни
+}
+
+double proizvodnai(double x) {
+    return 3 * x * x + 76.9242 * x + 364.594; // производная уравнения
+}
+
+
+double f(double x) {
+    return x * x * x *x - 2 * x * x * x - 7 * x * x + 8 * x +1; // уравнение, для которого ищем корни
+}
+
+double proizvodnai(double x) {
+    return 4 * x * x * x - 6 * x * x - 14 * x; // производная уравнения
+}
+
+
+double f(double x) {
+    return 10 * x - 4; // уравнение, для которого ищем корни
+}
+
+double proizvodnai(double x) {
+    return  10; // производная уравнения
+}
+
+double f(double x) {
+    return x * x * x + 8 * x + 10; // уравнение, для которого ищем корни
+}
+
+double proizvodnai(double x) {
+    return  3 * x * x + 8; // производная уравнения
+}
+*/
---
+++ MCHA/Laba4/L4/L4/L4.cpp
@@ -0,0 +1,244 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double f1(double x, double y) {
+    return 2*x-y-8;
+}
+
+double f2(double x, double y) {
+    return 3*x+2*y-5;
+}
+
+double df1_dx(double x, double y) {
+    return  2;
+}
+
+double df1_dy(double x, double y) {
+    return -1;
+}
+
+double df2_dx(double x, double y) {
+    return 3;
+}
+
+double df2_dy(double x, double y) {
+    return 2;
+}
+
+void simpleIterationMethod(double x0, double y0, double epsilon) {
+    double x = x0, y = y0;
+    double x_prev, y_prev;
+    int iterations = 0;
+
+    do {
+        x_prev = x;
+        y_prev = y;
+
+        double det = df1_dx(x_prev, y_prev) * df2_dy(x_prev, y_prev) - df1_dy(x_prev, y_prev) * df2_dx(x_prev, y_prev);
+
+        x = x_prev - (f1(x_prev, y_prev) * df2_dy(x_prev, y_prev) - f2(x_prev, y_prev) * df1_dy(x_prev, y_prev)) / det;
+        y = y_prev - (f2(x_prev, y_prev) * df1_dx(x_prev, y_prev) - f1(x_prev, y_prev) * df2_dx(x_prev, y_prev)) / det;
+
+        iterations++;
+    } while (abs(x - x_prev) > epsilon || abs(y - y_prev) > epsilon);
+
+    cout << "Simple Iteration Method:" << endl;
+    cout << "x = " << x << ", y = " << y << endl;
+    cout << "Iterations: " << iterations << endl;
+}
+
+void newtonMethod(double x0, double y0, double epsilon) {
+    double x = x0, y = y0;
+    double x_prev, y_prev;
+    int iterations = 0;
+
+    do {
+        x_prev = x;
+        y_prev = y;
+
+        double det = df1_dx(x_prev, y_prev) * df2_dy(x_prev, y_prev) - df1_dy(x_prev, y_prev) * df2_dx(x_prev, y_prev);
+
+        x = x_prev - (f1(x_prev, y_prev) * df2_dy(x_prev, y_prev) - f2(x_prev, y_prev) * df1_dy(x_prev, y_prev)) / det;
+        y = y_prev - (f2(x_prev, y_prev) * df1_dx(x_prev, y_prev) - f1(x_prev, y_prev) * df2_dx(x_prev, y_prev)) / det;
+
+        iterations++;
+    } while (abs(x - x_prev) > epsilon || abs(y - y_prev) > epsilon);
+
+    cout << "Newton's Method:" << endl;
+    cout << "x = " << x << ", y = " << y << endl;
+    cout << "Iterations: " << iterations << endl;
+}
+
+int main() {
+    double x0, y0, epsilon;
+
+    cout << "Enter initial values (x0 y0): ";
+    cin >> x0 >> y0;
+
+    cout << "Enter the desired precision: ";
+    cin >> epsilon;
+
+    simpleIterationMethod(x0, y0, epsilon);
+    newtonMethod(x0, y0, epsilon);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+/* 
+Само задание
+//Функция 1
+    f = [](double x)
+    {
+        double y = sqrt(2-x*x)/4;
+        return y;
+    };
+    function_point_cloud(f, Data_, x_min, x_max, step);
+    funcs.push_back(Data_);
+
+
+    //Функция 2
+    Data_.clear();
+    fp = [](double x)
+    {
+        double y = (x-0,3)/tan(x);
+        return  y;
+    };
+    function_point_cloud(fp, Data_, x_min, x_max, step);
+    funcs.push_back(Data_);
+    
+    
+    
+double f1(double x, double y) {
+    return tan(x * y) + 0.3 - x;
+}
+
+double f2(double x, double y) {
+    return 0.5 * pow(x, 2) + 2 * pow(y, 2) - 1;
+}
+
+double df1_dx(double x, double y) {
+    return -tan(x * y) + 1;
+}
+
+double df1_dy(double x, double y) {
+    return -x * pow(1 / cos(x * y), 2);
+}
+
+double df2_dx(double x, double y) {
+    return x;
+}
+
+double df2_dy(double x, double y) {
+    return 4 * y;
+}
+    
+    
+    
+    Пример1
+
+     //Функция 1
+    f = [](double x)
+    {
+        double y = (7-x)/5;
+        return y;
+    };
+    function_point_cloud(f, Data_, x_min, x_max, step);
+    funcs.push_back(Data_);
+
+
+    //Функция 2
+    Data_.clear();
+    fp = [](double x)
+    {
+        double y = (3*x+4)/2;
+        return  y;
+    };
+    function_point_cloud(fp, Data_, x_min, x_max, step);
+    funcs.push_back(Data_);
+    
+    
+double f1(double x, double y) {
+    return x+5*y-7;
+}
+
+double f2(double x, double y) {
+    return 3*x-2*y-4;
+}
+
+double df1_dx(double x, double y) {
+    return  1;
+}
+
+double df1_dy(double x, double y) {
+    return 5;
+}
+
+double df2_dx(double x, double y) {
+    return 3;
+}
+
+double df2_dy(double x, double y) {
+    return -2;
+}
+    
+
+
+    Пример 2
+     //Функция 1
+    f = [](double x)
+    {
+        double y = 2*x-8;
+        return y;
+    };
+    function_point_cloud(f, Data_, x_min, x_max, step);
+    funcs.push_back(Data_);
+
+
+    //Функция 2
+    Data_.clear();
+    fp = [](double x)
+    {
+        double y = (5 - 3*x)/2;
+        return  y;
+    };
+    function_point_cloud(fp, Data_, x_min, x_max, step);
+    funcs.push_back(Data_);
+    
+
+
+    double f1(double x, double y) {
+    return 2*x-y-8;
+}
+
+double f2(double x, double y) {
+    return 3*x+2*y-5;
+}
+
+double df1_dx(double x, double y) {
+    return  2;
+}
+
+double df1_dy(double x, double y) {
+    return -1;
+}
+
+double df2_dx(double x, double y) {
+    return 3;
+}
+
+double df2_dy(double x, double y) {
+    return 2;
+}
+    
+    */
