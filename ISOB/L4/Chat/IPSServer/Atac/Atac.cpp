7287b32f7ed958e4343891770141cdc9c829aa56
Lavrenovaanastasia <125998621+Lavrenovaanastasia@users.noreply.github.com>
2025-03-27 13:16:18 +0300

    Add files via upload

---
+++ ISOB/L4/Chat/IPSServer/Atac/Atac.cpp
@@ -0,0 +1,49 @@
+﻿#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+#include <thread>
+
+#pragma comment(lib, "ws2_32.lib")
+
+using namespace std;
+
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 5000
+
+void Attack(const char* serverIP, int serverPort) {
+    for (int i = 0; i < 10; ++i) {  // 10 попыток подключений
+        SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+        if (clientSocket == INVALID_SOCKET) {
+            cerr << "Ошибка создания сокета." << endl;
+            continue;
+        }
+
+        sockaddr_in serverAddr{};
+        serverAddr.sin_family = AF_INET;
+        serverAddr.sin_port = htons(serverPort);
+        inet_pton(AF_INET, serverIP, &serverAddr.sin_addr);
+
+        if (connect(clientSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+            cerr << "Не удалось подключиться к серверу!" << endl;
+        }
+        else {
+            cout << "Успешное подключение!" << endl;
+        }
+
+        closesocket(clientSocket);
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        cerr << "Ошибка инициализации Winsock." << endl;
+        return 1;
+    }
+
+    Attack(SERVER_IP, SERVER_PORT);
+
+    WSACleanup();
+    return 0;
+}
---
+++ ISOB/L4/Chat/IPSServer/IPSClient/IPSClient.cpp
@@ -0,0 +1,80 @@
+﻿#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+#include <thread>
+#include <string>
+#include <algorithm>
+
+#pragma comment(lib, "ws2_32.lib")
+
+using namespace std;
+
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 5000
+
+void ReceiveMessages(SOCKET clientSocket) {
+    char buffer[1024];
+    int bytesRead;
+    while ((bytesRead = recv(clientSocket, buffer, sizeof(buffer) - 1, 0)) != SOCKET_ERROR) {
+        if (bytesRead == 0) break;  
+        buffer[bytesRead] = '\0';
+        string receivedMessage(buffer);
+        receivedMessage.erase(remove(receivedMessage.begin(), receivedMessage.end(), '\r'), receivedMessage.end());
+        cout << receivedMessage << endl;
+    }
+}
+
+void SendMessages(SOCKET clientSocket) {
+    string message;
+    while (true) {
+        getline(cin, message);
+        if (message.empty()) continue;
+        send(clientSocket, message.c_str(), message.size(), 0);
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        cerr << "Ошибка инициализации Winsock." << endl;
+        return 1;
+    }
+
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == INVALID_SOCKET) {
+        cerr << "Ошибка создания сокета." << endl;
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in serverAddr{};
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(SERVER_PORT);
+    inet_pton(AF_INET, SERVER_IP, &serverAddr.sin_addr);
+
+    if (connect(clientSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        cerr << "Не удалось подключиться к серверу!" << endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    cout << "Подключено к серверу." << endl;
+
+    thread receiveThread(ReceiveMessages, clientSocket);
+    receiveThread.detach();  // Поток для получения сообщений
+
+    thread sendThread(SendMessages, clientSocket);  
+    sendThread.detach();
+
+   
+    while (true) {
+       
+    }
+
+    closesocket(clientSocket);
+    WSACleanup();
+    return 0;
+}
